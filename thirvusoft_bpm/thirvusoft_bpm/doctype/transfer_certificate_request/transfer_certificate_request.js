2bfb234b0c7da8dc75f83a6460dfb35b5f231c30
kponsakthivel <kponsakthivel@gmail.com>
2025-04-24 20:45:19 +0530

    financial info

--- thirvusoft_bpm/thirvusoft_bpm/doctype/transfer_certificate_request/transfer_certificate_request.js
+++ thirvusoft_bpm/thirvusoft_bpm/doctype/transfer_certificate_request/transfer_certificate_request.js
@@ -6,3 +6,15 @@
 
 // 	},
 // });
+frappe.ui.form.on('Transfer Certificate Request', {
+    refresh(frm) {
+        if (
+            frm.doc.docstatus === 1 &&
+            frm.doc.seat_retained &&
+            frm.doc.custom_seat_retain_upto
+        ) {
+            frappe.msgprint(`Seat will be retained until <b>${frappe.format_date(frm.doc.custom_seat_retain_upto)}</b>`);
+        }
+    }
+});
+
