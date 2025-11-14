4aae5d89b3b17a8c76faa267a3ccdde664a95377
PONSAKTHI VEL <154320941+kponsakthivel@users.noreply.github.com>
2025-11-14 12:03:08 +0530

    Disable Razorpay Checkout context override
    Comment out Razorpay Checkout context override.

--- thirvusoft_bpm/__init__.py
+++ thirvusoft_bpm/__init__.py
@@ -19,7 +19,7 @@ __version__ = '0.0.1'
 # from payments.templates.pages import razorpay_checkout
 # from thirvusoft_bpm.thirvusoft_bpm.code_backup.razorpay_checkout import gets_context
 # razorpay_checkout.get_context = gets_context
-
+#
 # # Monkeypatch Razorpay authorize/capture
 # from thirvusoft_bpm.patches.custom_razorpay_setting import monkey_patch_razorpay
 # monkey_patch_razorpay()
