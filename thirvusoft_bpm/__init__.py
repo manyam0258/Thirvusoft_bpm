2a2991cf18356935b37f7bf13e305e0c79890314
PONSAKTHI VEL <154320941+kponsakthivel@users.noreply.github.com>
2025-11-14 11:51:08 +0530

    Comment out Razorpay integration code
    Comment out Razorpay checkout context overrides and monkey patching.

--- thirvusoft_bpm/__init__.py
+++ thirvusoft_bpm/__init__.py
@@ -14,13 +14,13 @@
 # #
 
 __version__ = '0.0.1'
-import payments
-# Override Razorpay Checkout context
-from payments.templates.pages import razorpay_checkout
-from thirvusoft_bpm.thirvusoft_bpm.code_backup.razorpay_checkout import gets_context
-razorpay_checkout.get_context = gets_context
+# import payments
+# # Override Razorpay Checkout context
+# from payments.templates.pages import razorpay_checkout
+# from thirvusoft_bpm.thirvusoft_bpm.code_backup.razorpay_checkout import gets_context
+# razorpay_checkout.get_context = gets_context
 
-# Monkeypatch Razorpay authorize/capture
-from thirvusoft_bpm.patches.custom_razorpay_setting import monkey_patch_razorpay
-monkey_patch_razorpay()
+# # Monkeypatch Razorpay authorize/capture
+# from thirvusoft_bpm.patches.custom_razorpay_setting import monkey_patch_razorpay
+# monkey_patch_razorpay()
 
