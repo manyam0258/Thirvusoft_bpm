912b5ce2ea1622bafbb3e816f360d697514999ac
PONSAKTHI VEL <154320941+kponsakthivel@users.noreply.github.com>
2025-11-14 16:20:50 +0530

    Refactor payment entry override in hooks.py
    Removed commented-out code for 'Payment Entry' and updated its entry.

--- thirvusoft_bpm/hooks.py
+++ thirvusoft_bpm/hooks.py
@@ -101,10 +101,11 @@ after_migrate = "thirvusoft_bpm.thirvusoft_bpm.custom.py.customisation.payment_e
 override_doctype_class = {
 	"Payment Gateway Account": "thirvusoft_bpm.thirvusoft_bpm.custom.py.payment_gateway_account.Autoname",
     "Program Enrollment Tool": "thirvusoft_bpm.thirvusoft_bpm.custom.py.program_enrollment.CustomEnrollment",
-	"Payment Entry":{
-    	"thirvusoft_bpm.thirvusoft_bpm.custom.py.payment_entry.CustomPayment",
-    	"thirvusoft_bpm.thirvusoft_bpm.custom.py.payment_entry.CustomPaymentEntry"
-	},
+	# "Payment Entry":{
+ #    	"thirvusoft_bpm.thirvusoft_bpm.custom.py.payment_entry.CustomPayment",
+ #    	"thirvusoft_bpm.thirvusoft_bpm.custom.py.payment_entry.CustomPaymentEntry"
+	# },
+	"Payment Entry": "thirvusoft_bpm.thirvusoft_bpm.custom.py.payment_entry.CustomPaymentEntry"
 	'Payment Request': "thirvusoft_bpm.thirvusoft_bpm.custom.py.payment_request.CustomPaymentRequest",
 	"Student" : "thirvusoft_bpm.thirvusoft_bpm.custom.py.student.CustomStudent"
 
