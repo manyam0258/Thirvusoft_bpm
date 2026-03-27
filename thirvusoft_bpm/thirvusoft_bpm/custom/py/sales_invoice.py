e569e8df152b9cb54bb60ad01001d1df5395db59
PONSAKTHI VEL <154320941+kponsakthivel@users.noreply.github.com>
2026-03-27 21:24:06 +0530

    Refactor discount and installment type logic
    Refactor discount handling and installment type logic in sales invoice.

--- thirvusoft_bpm/thirvusoft_bpm/custom/py/sales_invoice.py
+++ thirvusoft_bpm/thirvusoft_bpm/custom/py/sales_invoice.py
@@ -149,15 +149,39 @@ def create_payment_request(list_of_docs=None):
         doc.payment_request_type = 'Inward' 
         customer = invoice_doc.customer
 
+        # discount_exists = frappe.db.exists("Discount", {"customer": customer})
+
+        # #if discount_exists:
+        #  #   doc.print_format = "TFEE-MKRE-DISC"
+        # #else:
+        #  #   doc.print_format = "TFEE-MKRE"
+        #         # Check for Discount and Installment Type
+        # installment_type = invoice_doc.get("installment_type") or ""
+
+        # if "Full Year" in installment_type:
+        #     if discount_exists:
+        #         doc.print_format = "DISC-FEE-PD-FY"
+        #     else:
+        #         doc.print_format = "FY-FEE-PD"
+        
+        # elif any(x in installment_type for x in ["Installment I", "Installment II", "Installment III"]):
+        #     if discount_exists:
+        #         doc.print_format = "TM-FEE-DISC-PD"
+        #     else:
+        #         doc.print_format = "TermFee"
+        
+        # else:
+        #     # Fallback to a default if no conditions are met
+        #     doc.print_format = "TermFee"
+
         discount_exists = frappe.db.exists("Discount", {"customer": customer})
 
-        #if discount_exists:
-         #   doc.print_format = "TFEE-MKRE-DISC"
-        #else:
-         #   doc.print_format = "TFEE-MKRE"
-                # Check for Discount and Installment Type
-        installment_type = invoice_doc.get("installment_type") or ""
-
+        # Get correct field
+        installment_type = invoice_doc.get("custom_installment_type") or ""
+        
+        # Normalize (optional but safer)
+        installment_type = installment_type.strip()
+        
         if "Full Year" in installment_type:
             if discount_exists:
                 doc.print_format = "DISC-FEE-PD-FY"
@@ -169,10 +193,6 @@ def create_payment_request(list_of_docs=None):
                 doc.print_format = "TM-FEE-DISC-PD"
             else:
                 doc.print_format = "TermFee"
-        
-        else:
-            # Fallback to a default if no conditions are met
-            doc.print_format = "TermFee"
 
         # doc.print_format = frappe.db.get_value(
         #     "Property Setter",
